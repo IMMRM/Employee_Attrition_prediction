ad9d434bed5875fa353f7940f7cbd79b0c568646
Immrm <mrmirdha@outlook.com>
2024-02-06 20:44:58 +0530

    Model ready

---
+++ config.py
@@ -0,0 +1,16 @@
+#This is the configuration file where we declare all the paths that will be used in project.
+
+#path to train_raw_data
+train_raw_data="../data/train.csv"
+#path to test_raw_data
+test_raw_data="../data/test.csv"
+#path to sample
+sample_path="../data/sample_submission.csv"
+#path to processed_data
+input_data="../input/file.csv"
+#path to test
+test_data="../test/"
+#feature info file
+feature_info="../data/FeatureInfo.csv"
+#output test file
+test_result_data="../result_test.csv"
